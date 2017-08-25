c21a6a1005a42a5290110fb99c50a06724962756
Dima Kogan <Dmitriy.Kogan@jpl.nasa.gov>
2017-08-25 13:14:44 -0700

    setup.py now runs ONLY from the Makefile
    This is required to properly handle the dependencies of the source file being
    built

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@
 
 from setuptools import setup
 from distutils.core import Extension
+import os
+
+makelevel = os.environ.get('MAKELEVEL')
+if makelevel is None or int(makelevel) < 1:
+    raise Exception("Please do not run setup.py directly. It needs the Makefile to handle dependencies")
 
 setup(name         = 'mrcal',
       version      = '0.1',
