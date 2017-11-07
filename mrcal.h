274501975d5bdbc60f84458a02b6dd3d8e281c81
Dima Kogan <Dmitriy.Kogan@jpl.nasa.gov>
2017-11-07 14:30:27 -0800

    using basic_points.h instead of duplicating it

--- mrcal.h
+++ mrcal.h
@@ -2,24 +2,8 @@
 
 #include <stdbool.h>
 
+#include "basic_points.h"
 
-union point2_t
-{
-    struct
-    {
-        double x,y;
-    };
-    double xy[2];
-};
-
-union point3_t
-{
-    struct
-    {
-        double x,y,z;
-    };
-    double xyz[3];
-};
 
 
 
