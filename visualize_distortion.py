6355e354158ff610991e3f6ed2b158f0371369d7
Dima Kogan <Dmitriy.Kogan@jpl.nasa.gov>
2018-02-14 18:30:18 -0800

    visualize_distortion.py now takes commandline options, has docs, etc

--- visualize_distortion.py
+++ visualize_distortion.py
@@ -1,10 +1,30 @@
 #!/usr/bin/python2
 
+r'''Renders a vector field to visualize the effect of a model
+
+Synopsis:
+
+  $ visualize_distortion.py left.cameramodel
+  ... a plot pops up showing the vector field
+
+This allows us to visually see what a distortion model does. Depending on the
+model, the vectors could be very large or very small, and we can scale them by
+passing '--scale s'. By default we sample in a 20x20 grid, but this spacing can
+be controlled by passing '--gridn N'.
+
+A tool with similar usefulness is undistort.py. That tool removes the distortion
+from a given set of images.
+
+'''
+
+
 import numpy as np
 import numpysane as nps
 import gnuplotlib as gp
 import sys
 import re
+import argparse
+import os
 
 from mrcal import cameramodel
 from mrcal import cahvor
@@ -13,66 +33,75 @@ from mrcal import projections
 
 
 
-def visualize_distortion_vector_field(model):
-    r'''Visualize the distortion effect of a set of intrinsic
-
-    This function renders the distortion vector field
-    '''
-
-    intrinsics = model.intrinsics()
-
-    N = 20
-    W,H = [2*center for center in intrinsics[1][2:4]]
-
-    # get the input and output grids of shape Nwidth,Nheight,2
-    grid, dgrid = projections.distortion_map__to_warped(intrinsics,
-                                                        np.linspace(0,W,N),
-                                                        np.linspace(0,H,N))
-
-    # shape: N*N,2
-    grid  = nps.clump(grid,  n=2)
-    dgrid = nps.clump(dgrid, n=2)
-
-    delta = dgrid-grid
-#    delta *= 1000
-    gp.plot( (grid[:,0], grid[:,1], delta[:,0], delta[:,1],
-              {'with': 'vectors size screen 0.01,20 fixed filled',
-               'tuplesize': 4,
-               }),
-             (grid[:,0], grid[:,1],
-              {'with': 'points',
-               'tuplesize': 2,
-               }),
-             _xrange=(0,W), _yrange=(H,0))
-
-    import time
-    time.sleep(100000)
 
 
 
-modelfile = sys.argv[1]
-if re.match(".*\.cahvor$", modelfile):
-    model = cahvor.read(modelfile)
+
+def parse_args():
+
+    parser = \
+        argparse.ArgumentParser(description = __doc__,
+                                formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('--gridn',
+                        type=int,
+                        default = 20,
+                        help='''How dense the vector field should be. By default we report a 20x20 grid''')
+
+    parser.add_argument('--scale',
+                        type=float,
+                        default = 1.0,
+                        help='''Scale the vectors by this factor. Default is 1.0 (report the truth), but this is often too small to see''')
+
+    parser.add_argument('model',
+                        type=lambda f: f if os.path.isfile(f) else \
+                                parser.error("The cameramodel must be an existing readable file, but got '{}".format(f)),
+                        nargs=1,
+                        help='''Input camera model. Assumed to be mrcal native, Unless the name is xxx.cahvor,
+                        in which case the cahvor format is assumed''')
+
+    return parser.parse_args()
+
+
+
+args = parse_args()
+if re.match(".*\.cahvor$", args.model[0]):
+    model = cahvor.read(args.model[0])
 else:
-    model = cameramodel(modelfile)
-
-try:
-    imagefile = sys.argv[2]
-except:
-    imagefile = None
+    model = cameramodel(args.model[0])
 
 
-if imagefile is None:
-    # no image file is given. Draw the vector field
-    visualize_distortion_vector_field(model)
-else:
-    m = re.match("(.*)\.([a-z][a-z][a-z])$", imagefile)
-    if not m:
-        raise Exception("imagefile must end in .xxx where 'xxx' is some image extension. Instead got '{}'".format(imagefile))
+intrinsics = model.intrinsics()
 
-    image_corrected = \
-        projections.undistort_image(model, imagefile)
+dims = model.dimensions()
+if dims is None:
+    sys.stderr.write("Warning: imager dimensions not available. Using centerpixel*2\n")
+    dims = model.intrinsics()[1][2:4] * 2
+W,H = dims
 
-    imagefile_corrected = "{}_undistorted.{}".format(m.group(1),m.group(2))
-    cv2.imwrite(imagefile_corrected, image_corrected)
-    print "Wrote {}".format(imagefile_corrected)
+# get the input and output grids of shape Nwidth,Nheight,2
+grid, dgrid = projections.distortion_map__to_warped(intrinsics,
+                                                    np.linspace(0,W,args.gridn),
+                                                    np.linspace(0,H,args.gridn))
+
+# shape: gridn*gridn,2
+grid  = nps.clump(grid,  n=2)
+dgrid = nps.clump(dgrid, n=2)
+
+delta = dgrid-grid
+delta *= args.scale
+gp.plot( (grid[:,0], grid[:,1], delta[:,0], delta[:,1],
+          {'with': 'vectors size screen 0.005,10 fixed filled',
+           'tuplesize': 4,
+           }),
+         (grid[:,0], grid[:,1],
+          {'with': 'points',
+           'tuplesize': 2,
+           }),
+         _xrange=(-50,W+50),
+         _yrange=(H+50, -50),
+         _set='object 1 rectangle from 0,0 to {},{} fillstyle empty'. \
+         format(W,H))
+
+import time
+time.sleep(100000)
