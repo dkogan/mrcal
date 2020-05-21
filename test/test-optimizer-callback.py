a230d1fc99c3a64e16889a2c277b7d733378f987
Dima Kogan <Dmitriy.Kogan@jpl.nasa.gov>
2020-05-21 00:58:22 -0700

    test-optimizer-callback is more sophisticated

--- test/test-optimizer-callback.py
+++ test/test-optimizer-callback.py
@@ -58,16 +58,19 @@ imagersizes = nps.cat(models[0].imagersize(),
 frames = linspace_shaped(3,6)
 frames[:,5] += 5 # push them back
 
-points                      = 10. + 2.*linspace_shaped(2,3)
-observations_point_xy       = 1000. + 500. * linspace_shaped(2,2)
-observations_point_weights  = np.array(((0.9,), (0.8,)))
-observations_point_distance = np.array(((10.,), (-1,)))
+points                      = 10. + 2.*linspace_shaped(3,3)
+observations_point_xy       = 1000. + 500. * linspace_shaped(5,2)
+observations_point_weights  = np.array((0.9, 0.8, 0.9, 1.3, 1.8))
+observations_point_distance = np.array((-1, -1, 100.0, 123.0, 443.9))
 observations_point          = nps.glue(observations_point_xy,
-                                       observations_point_weights,
-                                       observations_point_distance,
+                                       nps.transpose(observations_point_weights),
+                                       nps.transpose(observations_point_distance),
                                        axis = -1)
-indices_point_camintrinsics_camextrinsics = np.array(((0,1, 0),
-                                                      (1,0,-1)),
+indices_point_camintrinsics_camextrinsics = np.array(((0,1,-1),
+                                                      (1,0,-1),
+                                                      (1,1, 0),
+                                                      (2,0,-1),
+                                                      (2,1, 0)),
                                                      dtype = np.int32)
 
 all_test_kwargs = ( dict(do_optimize_intrinsic_core        = False,
