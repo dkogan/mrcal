1f3034e230fa542d0d76a71161220ae662e673d5
Dima Kogan <Dmitriy.Kogan@jpl.nasa.gov>
2020-08-18 00:21:38 -0700

    show_projection_uncertainty() draws contours until cbmax

--- mrcal/utils.py
+++ mrcal/utils.py
@@ -2056,7 +2056,7 @@ into a variable, even if you're not going to be doing anything with this object
                           'view map',
                           'contour surface',
                           'key box opaque',
-                          'cntrparam levels incremental 3,-0.5,0'])
+                          f'cntrparam levels incremental {cbmax},-0.5,0'])
 
     plot_data_args = [(err,
                        dict( tuplesize=3,
