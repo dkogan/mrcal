0c628dee76b9cc777d2c3a715bf71f8178dd8f74
Dima Kogan <Dmitriy.Kogan@jpl.nasa.gov>
2020-10-18 02:16:40 -0700

    more tests to test-projection-diff

--- test/test-projection-diff.py
+++ test/test-projection-diff.py
@@ -18,20 +18,24 @@ import mrcal
 import testutils
 
 
-model       = mrcal.cameramodel(f"{testdir}/data/cam0.splined.cameramodel")
-gridn_width = 50
+model_opencv8 = mrcal.cameramodel(f"{testdir}/data/cam0.opencv8.cameramodel")
+model_splined = mrcal.cameramodel(f"{testdir}/data/cam0.splined.cameramodel")
+gridn_width   = 50
 
-# Compare the model to itself. I should get 0 diff and identity transform
+########## Compare the model to itself. I should get 0 diff and identity transform
 difflen, diff, q0, implied_Rt10 = \
-    mrcal.projection_diff( (model,model),
+    mrcal.projection_diff( (model_splined,model_splined),
                            gridn_width       = gridn_width,
                            distance          = None,
                            use_uncertainties = False )
 
 testutils.confirm_equal( difflen.shape[1], gridn_width,
                          msg = "Expected number of columns" )
-testutils.confirm_equal( difflen.shape[0], int(round( model.imagersize()[1] / model.imagersize()[0] * gridn_width)),
+testutils.confirm_equal( difflen.shape[0], int(round( model_splined.imagersize()[1] / model_splined.imagersize()[0] * gridn_width)),
                          msg = "Expected number of rows" )
+
+icenter = np.array(difflen.shape) // 2
+
 
 testutils.confirm_equal( difflen*0, difflen,
                          eps = 0.04,
@@ -48,7 +52,7 @@ testutils.confirm_equal( 0, nps.mag(implied_Rt10[3,:]),
                          msg = "diff(model,model) at infinity should produce a rotation of 0 m")
 
 difflen, diff, q0, implied_Rt10 = \
-    mrcal.projection_diff( (model,model),
+    mrcal.projection_diff( (model_splined,model_splined),
                            gridn_width       = 50,
                            distance          = 3.,
                            use_uncertainties = False )
@@ -66,5 +70,31 @@ testutils.confirm_equal( 0, np.arccos((np.trace(implied_Rt10[:3,:]) - 1) / 2.) *
 testutils.confirm_equal( 0, nps.mag(implied_Rt10[3,:]),
                          eps = 0.01,
                          msg = "diff(model,model) at 3m should produce a rotation of 0 m")
+
+
+########## Check outlier handling when computing diffs without uncertainties.
+########## The model may only fit in one regions. Using data outside of that
+########## region poisons the solve unless we treat those measurements as
+########## outliers. This is controlled by the f_scale parameter in
+########## implied_Rt10__from_unprojections().
+difflen, diff, q0, implied_Rt10 = \
+    mrcal.projection_diff( (model_opencv8,model_splined),
+                           gridn_width       = gridn_width,
+                           distance          = 5,
+                           use_uncertainties = False,
+                           focus_radius      = 800)
+testutils.confirm_equal( 0, difflen[icenter[0],icenter[1]],
+                         eps = 0.1,
+                         msg = "Low-enough diff with high focus_radius")
+
+difflen, diff, q0, implied_Rt10 = \
+    mrcal.projection_diff( (model_opencv8,model_splined),
+                           gridn_width       = gridn_width,
+                           distance          = 5,
+                           use_uncertainties = False,
+                           focus_radius      = 366)
+testutils.confirm_equal( 0, difflen[icenter[0],icenter[1]],
+                         eps = 0.1,
+                         msg = "Low-enough diff with low focus_radius")
 
 testutils.finish()
