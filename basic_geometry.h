607931f0bfb529af78e0886d886c91be40c55e8f
Dima Kogan <Dmitriy.Kogan@jpl.nasa.gov>
2020-11-21 20:07:30 -0800

    Commented basic_geometry.h

--- basic_geometry.h
+++ basic_geometry.h
@@ -1,14 +1,31 @@
 #pragma once
 
+// A 2D point or vector
+//
+// The individual elements can be accessed via .x and .y OR the vector can be
+// accessed as an .xy[] array:
+//
+//   mrcal_point2_t p = f();
+//
+// Now p.x and p.xy[0] refer to the same value.
 typedef union
 {
     struct
     {
         double x,y;
     };
+
     double xy[2];
 } mrcal_point2_t;
 
+// A 3D point or vector
+//
+// The individual elements can be accessed via .x and .y and .z OR the vector
+// can be accessed as an .xyz[] array:
+//
+//   mrcal_point3_t p = f();
+//
+// Now p.x and p.xy[0] refer to the same value.
 typedef union
 {
     struct
@@ -18,7 +35,7 @@ typedef union
     double xyz[3];
 } mrcal_point3_t;
 
-// unconstrained 6DOF pose containing a rodrigues rotation and a translation
+// Unconstrained 6DOF pose containing a Rodrigues rotation and a translation
 typedef struct
 {
     mrcal_point3_t r,t;
