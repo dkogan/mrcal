a601867bcdc7a44ad606435f3c67b187722283c0
Dima Kogan <Dmitriy.Kogan@jpl.nasa.gov>
2020-11-21 17:37:59 -0800

    added first cut at the org project definition

---
+++ doc/mrcal-docs-publish.el
@@ -0,0 +1,22 @@
+(defun dima-org-export-build-docs (plist)
+  (shell-command (format "make -C .. doc")))
+
+(setq org-publish-project-alist
+      '(("orgfiles"
+         :base-directory "."
+         :base-extension "org"
+         :publishing-directory "./out"
+         :publishing-function org-html-publish-to-html
+         :completion-function dima-org-export-build-docs
+         :section-numbers nil
+         :with-toc nil
+         :html-preamble nil)
+
+        ("images"
+         :base-directory "~/jpl/mrcaldocs/"
+         :base-extension "jpg\\|png\\|svg"
+         :publishing-directory "./out"
+         :publishing-function org-publish-attachment)
+
+        ("website"
+         :components ("orgfiles" "images"))))
