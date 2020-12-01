2dc98e738229fc35e2e913f040f286aab494a352
Dima Kogan <Dmitriy.Kogan@jpl.nasa.gov>
2020-11-30 21:05:31 -0800

    documentation: added styles and a nav bar at the top

--- doc/mrcal-docs-publish.el
+++ doc/mrcal-docs-publish.el
@@ -12,14 +12,52 @@
          :section-numbers nil
          :with-toc t
          :with-sub-superscript nil
+         :html-postamble nil
          :with-author "Dima Kogan"
          :with-email  "kogan@jpl.nasa.gov"
          :html-head ,(concat "<style>"
+                             "body         {background-color: burlywood;} "
+                             "pre          {border: #303030; box-shadow: 3px 3px 3px #606060;} "
                              "pre.src      {background-color: #303030; color: #e5e5e5; max-width 700px} "
                              "pre.example  {background-color: #303030; color: #e5e5e5; max-width 700px} "
+
                              ".org-svg     {min-width: 500px; max-width: 60%;} "
                              ".figure      {min-width: 500px; max-width: 60%;} "
+
+                             ;;  Style for the nav bar at the top. Adapted from
+                             ;;  o-blog.css
+                             ".supernav_title { "
+                             "    font-size: 30px; "
+                             "    font-weight: bolder; "
+                             "} "
+                             ".supernav_list { "
+                             "    vertical-align: middle; "
+                             "    top: 0; "
+                             "    list-style: none outside none; "
+                             "    white-space: nowrap; "
+                             "    overflow: hidden; "
+                             "} "
+                             ".supernav_list li { "
+                             "    display: inline-block; "
+                             "} "
+                             ".supernav_list li + li:before { "
+                             "    content: \" / \"; "
+                             "    padding: 0 10px; "
+                             "} "
+
                              "</style>")
+
+         :html-preamble ,(concat
+                          "<ul class=\"supernav_list\"> "
+                          "<li class=\"supernav_title\">mrcal</li> "
+                          "<li><a href=\"index.html\">Documentation index</a></li> "
+                          "<li><a href=\"tour.html\">A tour of mrcal</a></li> "
+                          "<li><a href=\"c-api.html\">C API</a></li> "
+                          "<li><a href=\"python-api.html\">Python API</a></li> "
+                          "<li><a href=\"commandline-tools.html\">Commandline tools</a></li> "
+                          "<li><a href=\"https://github.jpl.nasa.gov/maritime-robotics/mrcal\">Sources</a></li> "
+                          "</ul> <hr>")
+
          :html-mathjax-options ((path "MathJax-master/es5/tex-chtml.js")
                                 (scale "100")
                                 (align "center")
