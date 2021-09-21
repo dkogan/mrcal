6ce55acaf10325f776773857e3a120f79305a65f
Dima Kogan <dima@secretsauce.net>
2021-09-20 23:40:14 -0700

    --help typo fixes

--- test/test-triangulation-uncertainty.py
+++ test/test-triangulation-uncertainty.py
@@ -73,14 +73,14 @@ def parse_args():
     parser.add_argument('--q-calibration-stdev',
                         type    = float,
                         default = 0.0,
-                        help='''The observed pixel uncertainty of the chessboard
+                        help='''The observed_pixel_uncertainty of the chessboard
                         observations at calibration time. Defaults to 0.0. At
                         least one of --q-calibration-stdev and
                         --q-observation-stdev MUST be given as > 0''')
     parser.add_argument('--q-observation-stdev',
                         type    = float,
                         default = 0.0,
-                        help='''The observed pixel uncertainty of the point
+                        help='''The observed_pixel_uncertainty of the point
                         observations at triangulation time. Defaults to 0.0. At
                         least one of --q-calibration-stdev and
                         --q-observation-stdev MUST be given as > 0''')
