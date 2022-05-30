711a48a23877c6eefa3cc2c06f3346dc10d30bb5
Dima Kogan <dima@secretsauce.net>
2022-05-30 09:40:04 -0700

    optical flow logic cleanup
    - I compute the flow on a decimated image
    
    - I plot vectors on an even more decimated image

--- analyses/sfm-experiments/sfm.py
+++ analyses/sfm-experiments/sfm.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
 r'''
-s=5;
-mkdir -p scaled$s;
-parallel convert '{}' -scale $s'%' scaled$s/'{/}' ::: delta/*.jpg
 '''
 
 import sys
@@ -13,79 +10,33 @@ import gnuplotlib as gp
 import cv2
 import glob
 
-directory = "scaled5/*.jpg"
-D = 5
-do_sparse_features = True
 
-l = sorted(glob.glob(directory))
+def imread(filename, decimation):
+    image = cv2.imread(filename)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return image, image[::decimation, ::decimation]
 
-frame1 = cv2.imread(l[0])
-prvs   = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+def plot_flow(filename,
+              image, flow, decimation):
+    H,W = flow.shape[:2]
 
-H,W = frame1.shape[:2]
+    # each has shape (H,W)
+    xx,yy = np.meshgrid(np.arange(0,W,decimation),
+                        np.arange(0,H,decimation))
 
+    # shape (H,W,4)
+    vectors = \
+        nps.glue( nps.dummy(xx,-1),
+                  nps.dummy(yy,-1),
+                  flow[::decimation, ::decimation, :],
+                  axis = -1);
+    vectors = nps.clump(vectors, n=2)
 
-
-
-if do_sparse_features:
-    # params for ShiTomasi corner detection
-    feature_params = dict( maxCorners = 100,
-                           qualityLevel = 0.3,
-                           minDistance = 7,
-                           blockSize = 7 )
-    # Parameters for lucas kanade optical flow
-    lk_params = dict( winSize  = (15, 15),
-                      maxLevel = 2,
-                      criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
-
-    p0 = cv2.goodFeaturesToTrack(prvs, mask = None, **feature_params)
-    #mask = np.zeros_like(old_frame)
-
-
-
-i=0
-
-for f in l[1:]:
-    frame2 = cv2.imread(f)
-
-    next = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-    filename = f"/tmp/flow{i:03d}.png"
-
-    if not do_sparse_features:
-        # shape (H,W,2)
-        flow = -cv2.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-
-        # each has shape (H/D,W/D)
-        xx,yy = np.meshgrid(np.arange(0,W,D),
-                            np.arange(0,H,D))
-
-        # shape (H,W,4)
-        vectors = \
-            nps.glue( nps.dummy(xx,-1),
-                      nps.dummy(yy,-1),
-                      flow[0:H:D, 1:W:D, :],
-                      axis = -1);
-        vectors = nps.clump(vectors, n=2)
-
-    else:
-        # calculate optical flow
-        p1, st, err = cv2.calcOpticalFlowPyrLK(prvs, next, p0, None, **lk_params)
-        # Select good points
-        if p1 is not None:
-            good_new = p1[st==1]
-            good_old = p0[st==1]
-
-
-        d = good_new - good_old
-
-        vectors = nps.glue(good_new,d, axis=-1)
-        p0 = good_new.reshape(-1, 1, 2)
-
-
-    gp.plot( (next, dict(_with='image',
-                         tuplesize = 3)),
+    gp.plot( (image, dict(_with='image',
+                          tuplesize = 3)),
              (vectors,
-              dict(tuplesize=-4, _with='vectors lc "red"',)),
+              dict(_with='vectors lc "red"',
+                   tuplesize=-4)),
 
              _set  = 'palette gray',
              unset = 'colorbox',
@@ -94,8 +45,44 @@ for f in l[1:]:
              _yrange = (H,0),
              hardcopy = filename)
 
-
-
     print(f"Wrote {filename}")
-    prvs = next
+
+
+
+
+
+
+directory       = "/home/dima/data/xxxxx/delta/*.jpg"
+outdir          = "/tmp"
+decimation      = 20
+decimation_extra_plot = 5
+Nimages         = 10 #None # all of them
+
+
+l = sorted(glob.glob(directory))
+
+
+image0,image0_decimated = imread(l[0], decimation)
+
+i=0
+for f in l[1:Nimages]:
+    image1,image1_decimated = imread(f, decimation)
+
+    # shape (Hdecimated,Wdecimated,2)
+    flow = -cv2.calcOpticalFlowFarneback(image0_decimated, image1_decimated,
+                                         flow       = None, # for in-place output
+                                         pyr_scale  = 0.5,
+                                         levels     = 3,
+                                         winsize    = 15,
+                                         iterations = 3,
+                                         poly_n     = 5,
+                                         poly_sigma = 1.2,
+                                         flags      = 0# cv2.OPTFLOW_USE_INITIAL_FLOW
+                                         )
+
+    plot_flow(f"{outdir}/flow{i:03d}.png",
+              image0_decimated, flow,
+              decimation_extra_plot)
+
+    image0_decimated = image1_decimated
     i += 1
