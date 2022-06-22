6f19fdb57bb1c32eb142ddab474bcbb9a0c76a3c
Dima Kogan <dima@secretsauce.net>
2022-06-21 20:11:40 -0700

    keyword-only arg spec for optimize() and optimizer_callback() work now
    There was an uninteresting mistake

--- mrcal-pywrap.c
+++ mrcal-pywrap.c
@@ -1329,8 +1329,7 @@ PyObject* _optimize(bool is_optimize, // or optimizer_callback
                              OPTIMIZE_ARGUMENTS_OPTIONAL(NAMELIST)
                              NULL};
         if(!PyArg_ParseTupleAndKeywords( args, kwargs,
-                                         "$"
-                                         OPTIMIZE_ARGUMENTS_REQUIRED(PARSECODE) "|"
+                                         OPTIMIZE_ARGUMENTS_REQUIRED(PARSECODE) "|$"
                                          OPTIMIZE_ARGUMENTS_OPTIONAL(PARSECODE)
                                          ":mrcal.optimize",
 
@@ -1348,8 +1347,7 @@ PyObject* _optimize(bool is_optimize, // or optimizer_callback
                              OPTIMIZER_CALLBACK_ARGUMENTS_OPTIONAL_EXTRA(NAMELIST)
                              NULL};
         if(!PyArg_ParseTupleAndKeywords( args, kwargs,
-                                         "$"
-                                         OPTIMIZE_ARGUMENTS_REQUIRED(PARSECODE) "|"
+                                         OPTIMIZE_ARGUMENTS_REQUIRED(PARSECODE) "|$"
                                          OPTIMIZE_ARGUMENTS_OPTIONAL(PARSECODE)
                                          OPTIMIZER_CALLBACK_ARGUMENTS_OPTIONAL_EXTRA(PARSECODE)
                                          ":mrcal.optimizer_callback",
