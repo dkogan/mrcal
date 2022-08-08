f6666450e50c61085c9bbafa7413589c630b9a55
Dima Kogan <dima@secretsauce.net>
2022-08-07 18:28:11 -0700

    test-poseutils-lib: normalize rt to resolve a false-positive failing test

--- test/test-poseutils-lib.py
+++ test/test-poseutils-lib.py
@@ -120,6 +120,27 @@ def compose_Rt(Rt0, Rt1):
     t2 = nps.matmult(t1, nps.transpose(R0)) + t0
     return nps.glue( R2, t2.ravel(), axis=-2)
 
+def normalize_r(r):
+    r'''If abs(rot) > 180deg -> flip direction, abs(rot) <- 180-abs(rot)'''
+    norm2 = nps.norm2(r)
+    if norm2 < np.pi*np.pi:
+        return r
+
+    mag = np.sqrt(norm2)
+    r_unit = r / mag
+
+    mag = mag % (np.pi*2.)
+    if mag < np.pi:
+        # same direction, but fewer full rotations
+        return r_unit*mag
+
+    return -r_unit * (np.pi*2. - mag)
+
+def normalize_rt(rt):
+    return nps.glue(normalize_r(rt[:3]),
+                    rt[3:],
+                    axis=-1)
+
 def compose_r(r0, r1):
     r'''Simple reference implementation'''
     return r_from_R(nps.matmult( R_from_r(r0),
@@ -962,6 +983,9 @@ confirm_equal( Rt2,
 
 rt2 = mrcal.compose_rt(rt0_ref, rt1_ref,rt0_ref,
                        out=out6)
+# Needed here. The two rotations are semantically equivalent, but numerically
+# different
+rt2 = normalize_rt(rt2)
 confirm_equal( rt2,
                compose_rt(compose_rt(rt0_ref, rt1_ref), rt0_ref),
                msg='compose_rt with 3 inputs')
