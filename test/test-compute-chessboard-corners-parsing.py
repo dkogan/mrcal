bb86feba2b5230ad3b1c0378e4f05c5bb86cd0dd
Dima Kogan <dima@secretsauce.net>
2022-09-05 12:30:20 -0700

    test for the improved compute_chessboard_corners() logic

--- test/test-compute-chessboard-corners-parsing.py
+++ test/test-compute-chessboard-corners-parsing.py
@@ -70,11 +70,16 @@ indices_frame_camera_ref = np.array(((0,0),
                                      (2,1),
                                      ), dtype=np.int32)
 
-observations, indices_frame_camera, paths = \
-    mrcal.compute_chessboard_corners(Nw                = 2,
-                                     Nh                = 3,
-                                     globs             = ('frame*-cam1.jpg','frame*-cam2.jpg'),
-                                     corners_cache_vnl = io.StringIO(corners_all_or_none_noweight))
+try:
+    observations, indices_frame_camera, paths = \
+        mrcal.compute_chessboard_corners(Nw                = 2,
+                                         Nh                = 3,
+                                         globs             = ('frame*-cam1.jpg','frame*-cam2.jpg'),
+                                         corners_cache_vnl = io.StringIO(corners_all_or_none_noweight))
+except Exception as e:
+    observations         = f"Error: {e}"
+    indices_frame_camera = f"Error: {e}"
+
 
 testutils.confirm_equal( observations,
                          observations_ref,
@@ -111,9 +116,9 @@ frame101-cam2.jpg 31 32 0.24
 frame102-cam1.jpg - - -
 frame102-cam2.jpg 40 40 0.25
 frame102-cam2.jpg 41 40 0.26
-frame102-cam2.jpg 40 41 0.27
+frame102-cam2.jpg 40 41 -2
 frame102-cam2.jpg 41 41 0.28
-frame102-cam2.jpg 40 42 0.29
+frame102-cam2.jpg 40 42 -
 frame102-cam2.jpg 41 42 0.30
 '''
 
@@ -125,6 +130,8 @@ observations_ref_y[:] = np.arange(3)
 
 observations_ref_w = nps.clump(observations_ref[:,:,:,2], n=3)
 observations_ref_w[:] = (np.arange(30) + 1) / 100
+observations_ref[4,1,0,2] = -1.
+observations_ref[4,2,0,2] = -1.
 
 observations_ref_frame = nps.mv(observations_ref[..., :2], 0, -1)
 observations_ref_frame += np.arange(5)*10
@@ -136,12 +143,17 @@ indices_frame_camera_ref = np.array(((0,0),
                                      (2,1),
                                      ), dtype=np.int32)
 
-observations, indices_frame_camera, paths = \
-    mrcal.compute_chessboard_corners(Nw                = 2,
-                                     Nh                = 3,
-                                     globs             = ('frame*-cam1.jpg','frame*-cam2.jpg'),
-                                     corners_cache_vnl = io.StringIO(corners_all_or_none),
-                                     extracol          = 'weight')
+try:
+    observations, indices_frame_camera, paths = \
+        mrcal.compute_chessboard_corners(Nw                = 2,
+                                         Nh                = 3,
+                                         globs             = ('frame*-cam1.jpg','frame*-cam2.jpg'),
+                                         corners_cache_vnl = io.StringIO(corners_all_or_none),
+                                         extracol          = 'weight')
+
+except Exception as e:
+    observations         = f"Error: {e}"
+    indices_frame_camera = f"Error: {e}"
 
 testutils.confirm_equal( observations,
                          observations_ref,
@@ -150,5 +162,79 @@ testutils.confirm_equal( indices_frame_camera,
                          indices_frame_camera_ref,
                          msg = "indices_frame_camera all-or-none")
 
+
+
+corners_partial = r'''# filename x y weight
+frame100-cam1.jpg 0 0   0.01
+frame100-cam1.jpg 1 0   0.02
+frame100-cam1.jpg 0 1   0.03
+frame100-cam1.jpg 1 1   0.04
+frame100-cam1.jpg 0 2   0.05
+frame100-cam1.jpg 1 2   0.06
+frame100-cam2.jpg 10 10 0.07
+frame100-cam2.jpg 11 10 0.08
+frame100-cam2.jpg 10 11 0.09
+frame100-cam2.jpg 11 11 0.10
+frame100-cam2.jpg 10 12 0.11
+frame100-cam2.jpg 11 12 0.12
+frame101-cam1.jpg 20 20 0.13
+frame101-cam1.jpg 21 20 0.14
+frame101-cam1.jpg 20 21 0.15
+frame101-cam1.jpg 21 21 0.16
+frame101-cam1.jpg 20 22 0.17
+frame101-cam1.jpg 21 22 0.18
+frame101-cam2.jpg 30 30 0.19
+frame101-cam2.jpg 31 30 0.20
+frame101-cam2.jpg 30 31 0.21
+frame101-cam2.jpg 31 31 0.22
+frame101-cam2.jpg 30 32 0.23
+frame101-cam2.jpg 31 32 0.24
+frame102-cam1.jpg - - -
+frame102-cam2.jpg 40 40 0.25
+frame102-cam2.jpg 41 40 0.26
+frame102-cam2.jpg - - -2
+frame102-cam2.jpg 41 41 0.28
+frame102-cam2.jpg - - -
+frame102-cam2.jpg 41 42 0.30
+'''
+
+observations_ref = np.empty((5,3,2,3), dtype=float)
+observations_ref_x = observations_ref[:,:,:,0]
+observations_ref_y = nps.transpose(observations_ref[:,:,:,1])
+observations_ref_x[:] = np.arange(2)
+observations_ref_y[:] = np.arange(3)
+
+observations_ref_frame = nps.mv(observations_ref[..., :2], 0, -1)
+observations_ref_frame += np.arange(5)*10
+
+observations_ref_w = nps.clump(observations_ref[:,:,:,2], n=3)
+observations_ref_w[:] = (np.arange(30) + 1) / 100
+observations_ref[4,1,0,:] = -1.
+observations_ref[4,2,0,:] = -1.
+
+indices_frame_camera_ref = np.array(((0,0),
+                                     (0,1),
+                                     (1,0),
+                                     (1,1),
+                                     (2,1),
+                                     ), dtype=np.int32)
+
+try:
+    observations, indices_frame_camera, paths = \
+        mrcal.compute_chessboard_corners(Nw                = 2,
+                                         Nh                = 3,
+                                         globs             = ('frame*-cam1.jpg','frame*-cam2.jpg'),
+                                         corners_cache_vnl = io.StringIO(corners_partial),
+                                         extracol          = 'weight')
+except Exception as e:
+    observations         = f"Error: {e}"
+    indices_frame_camera = f"Error: {e}"
+
+testutils.confirm_equal( observations,
+                         observations_ref,
+                         msg = "observations partial")
+testutils.confirm_equal( indices_frame_camera,
+                         indices_frame_camera_ref,
+                         msg = "indices_frame_camera partial")
 
 testutils.finish()
