6c2b4804302e71007ac395de2259108b46b03c0e
Dima Kogan <dima@secretsauce.net>
2023-02-22 00:31:23 -0800

    improved error message

--- mrcal/model_analysis.py
+++ mrcal/model_analysis.py
@@ -1293,7 +1293,7 @@ A tuple
                                                what       = 'worstdirection-stdev') \
                   for i in range(len(models)) ]
         except Exception as e:
-            print(f"WARNING: projection_diff() was asked to use uncertainties, but they aren't available/couldn't be computed. Falling back on the region-based-only logic\nException: {e}",
+            print(f"WARNING: projection_diff() was asked to use uncertainties, but they aren't available/couldn't be computed. Falling back on the region-based-only logic. Caught exception: {e}",
                   file = sys.stderr)
 
     if focus_center is None:
