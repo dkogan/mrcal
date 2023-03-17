37ae55dd066acb83eabf0d5123eab384be41e3bc
Dima Kogan <dima@secretsauce.net>
2023-03-17 11:11:19 -0700

    using newer mrbuild

--- choose_mrbuild.mk
+++ choose_mrbuild.mk
@@ -8,7 +8,7 @@ else ifneq (,$(wildcard /usr/include/mrbuild/Makefile.common.header))
   MRBUILD_MK=/usr/include/mrbuild
   MRBUILD_BIN=/usr/bin
 else
-  MRBUILD_VER := 1.1
+  MRBUILD_VER := 1.2
   URL         := https://github.com/dkogan/mrbuild/archive/refs/tags/v${MRBUILD_VER}.tar.gz
 
   cmd := wget ${URL} && sha512sum --quiet --strict -c mrbuild.checksums && tar xvfz v${MRBUILD_VER}.tar.gz && mv mrbuild-${MRBUILD_VER} mrbuild
