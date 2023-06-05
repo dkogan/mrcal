4d9249ea91fe67978b2288c6af980c42da0e79e8
Dima Kogan <dima@secretsauce.net>
2023-06-05 16:10:51 -0700

    choose_mrbuild checks the latest mrbuild and ONLY the latest mrbuild
    I was using sha512sum wrong. Now I only store the latest checksum only

--- choose_mrbuild.mk
+++ choose_mrbuild.mk
@@ -11,7 +11,7 @@ else
   MRBUILD_VER := 1.3
   URL         := https://github.com/dkogan/mrbuild/archive/refs/tags/v${MRBUILD_VER}.tar.gz
 
-  cmd := wget -O v${MRBUILD_VER}.tar.gz ${URL} && sha512sum --ignore-missing --quiet --strict -c mrbuild.checksums && tar xvfz v${MRBUILD_VER}.tar.gz && mv mrbuild-${MRBUILD_VER} mrbuild
+  cmd := wget -O v${MRBUILD_VER}.tar.gz ${URL} && sha512sum --quiet --strict -c mrbuild.checksums && tar xvfz v${MRBUILD_VER}.tar.gz && mv mrbuild-${MRBUILD_VER} mrbuild
 
   $(error mrbuild not found. Either 'apt install mrbuild', or if not possible, get it locally like this: '${cmd}')
 endif
