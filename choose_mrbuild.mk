7bcc4bc84944084f43864b1fb75f334c55006a1c
Dima Kogan <dima@secretsauce.net>
2023-06-29 12:55:10 -0700

    new choose_mrbuild

--- choose_mrbuild.mk
+++ choose_mrbuild.mk
@@ -8,10 +8,13 @@ else ifneq (,$(wildcard /usr/include/mrbuild/Makefile.common.header))
   MRBUILD_MK=/usr/include/mrbuild
   MRBUILD_BIN=/usr/bin
 else
-  MRBUILD_VER := 1.4
-  URL         := https://github.com/dkogan/mrbuild/archive/refs/tags/v${MRBUILD_VER}.tar.gz
+  V      := 1.5
+  SHA512 := 8b322fa41351d6b7a3f43c1e05929e4dbe4630f0bde3c9a1b7d6e3138ef8749853afa49cc800ff1c64453ac203e1128cf1ddbbda829100a6d1aeb3eb9f6fb7b0
 
-  cmd := wget -O v${MRBUILD_VER}.tar.gz ${URL} && sha512sum --quiet --strict -c mrbuild.checksums && tar xvfz v${MRBUILD_VER}.tar.gz && mv mrbuild-${MRBUILD_VER} mrbuild
+  URL   := https://github.com/dkogan/mrbuild/archive/refs/tags/v$V.tar.gz
+  TARGZ := mrbuild-$V.tar.gz
+
+  cmd := wget -O $(TARGZ) ${URL} && sha512sum --quiet --strict -c <(echo $(SHA512) $(TARGZ)) && tar xvfz $(TARGZ) && ln -fs mrbuild-$V mrbuild
 
   $(error mrbuild not found. Either 'apt install mrbuild', or if not possible, get it locally like this: '${cmd}')
 endif
