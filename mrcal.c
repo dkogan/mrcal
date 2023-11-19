1e2065d7e65b814885d66f5ef65104d58ebfffb4
Dima Kogan <dima@secretsauce.net>
2023-11-19 11:15:20 -0800

    Fixed memory leak in the outlier-rejection loop
    Fixes:
    
      https://github.com/dkogan/mrcal/issues/11
    
    Thanks to matthew.morley.ca@gmail.com for finding bug and identifying the fix

--- mrcal.c
+++ mrcal.c
@@ -6469,6 +6469,9 @@ mrcal_optimize( // out
         double outliernessScale = -1.0;
         do
         {
+            if(solver_context != NULL)
+                dogleg_freeContext(&solver_context);
+
             norm2_error = dogleg_optimize2(packed_state,
                                            Nstate, ctx.Nmeasurements, ctx.N_j_nonzero,
                                            (dogleg_callback_t*)&optimizer_callback, &ctx,
