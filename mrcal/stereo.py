dkogan/mrcal

commit 6f6212e740c61f9e16bcc69b1f38c6a7f5ffb6b0

mrcal.stereo_range() uses the C internals

mrcal/stereo.py
@@ -1207,6 +1207,74 @@ RETURNED VALUES
     if is_scalar:
         disparity = np.array((disparity,),)
 
+    Rt01 = mrcal.compose_Rt( models_rectified[0].extrinsics_Rt_fromref(),
+                             models_rectified[1].extrinsics_Rt_toref())
+    baseline = nps.mag(Rt01[3,:])
+
+    if qrect0 is None:
+        W,H = models_rectified[0].imagersize()
+        if np.any(disparity.shape - np.array((H,W),dtype=int)):
+            raise Exception(f"qrect0 is None, so the disparity image must have the full dimensions of a rectified image")
+
+        return                                    \
+            mrcal._mrcal_npsp._stereo_range_dense \
+                ( disparity_scaled     = disparity.astype(np.uint16),
+                  disparity_scale      = np.uint16(disparity_scale),
+                  disparity_scaled_min = np.uint16(disparity_min),
+                  disparity_scaled_max = np.uint16(np.iinfo(np.uint16).max),
+                  rectification_model_type = models_rectified[0].intrinsics()[0],
+                  fxycxy_rectified     = models_rectified[0].intrinsics()[1].astype(float),
+                  baseline             = baseline )
+
+    else:
+        return                                     \
+            mrcal._mrcal_npsp._stereo_range_sparse \
+                ( disparity            = disparity.astype(float) / disparity_scale,
+                  qrect0               = qrect0.astype(float),
+                  disparity_min        = float(disparity_min),
+                  disparity_max        = np.finfo(float).max,
+                  rectification_model_type = models_rectified[0].intrinsics()[0],
+                  fxycxy_rectified     = models_rectified[0].intrinsics()[1].astype(float),
+                  baseline             = baseline )
+
+    if is_scalar:
+        r = r[0]
+    return r
+
+
+def _stereo_range_python(disparity,
+                         models_rectified,
+                         *,
+                         disparity_scale = 1,
+                         disparity_min   = 0,
+                         qrect0          = None):
+
+    r'''Reference implementation of mrcal.stereo_range() in python
+
+The main implementation is written in C in stereo.c:
+
+  mrcal_stereo_range_sparse() and mrcal_stereo_range_dense()
+
+This should be identical to the stereo_range() function above. This is
+checked by the test-stereo-range.py test.
+
+    '''
+
+    _validate_models_rectified(models_rectified)
+
+    # I want to support scalar disparities. If one is given, I convert it into
+    # an array of shape (1,), and then pull it out at the end
+    is_scalar = False
+    try:
+        s = disparity.shape
+    except:
+        is_scalar = True
+    if not is_scalar:
+        if len(s) == 0:
+            is_scalar = True
+    if is_scalar:
+        disparity = np.array((disparity,),)
+
     W,H = models_rectified[0].imagersize()
     if qrect0 is None and np.any(disparity.shape - np.array((H,W),dtype=int)):
         raise Exception(f"qrect0 is None, so the disparity image must have the full dimensions of a rectified image")

mrcal-genpywrap.py
@@ -1008,4 +1008,145 @@ unsigned integers. The last row is the BGR color-mapped values.
             },
 )
 
+m.function( "_stereo_range_sparse",
+            """Internal wrapper of mrcal_stereo_range_sparse()
+""",
+
+            args_input       = ('disparity',
+                                'qrect0',
+                                'disparity_min',
+                                'disparity_max',
+                                'fxycxy_rectified',
+                                'baseline'
+                                ),
+            prototype_input  = (('N',),
+                                ('N',2,),
+                                (),
+                                (),
+                                (4,),
+                                ()),
+            prototype_output = ( 'N', ),
+
+            extra_args = (("const char*", "rectification_model_type", "NULL", "s"),),
+
+            Ccode_cookie_struct = '''
+              mrcal_lensmodel_type_t type;
+            ''',
+
+            Ccode_validate = r'''
+
+              cookie->type = mrcal_lensmodel_type_from_name(rectification_model_type);
+              // the mrcal_stereo_range_...() functions will check to make sure
+              // the type is valid
+
+              return CHECK_CONTIGUOUS_AND_SETERROR_ALL();''',
+
+            Ccode_slice_eval = \
+            {
+                (float,float,float,float,float,float,
+                 float): '''
+
+                 const int N = dims_slice__output[0];
+
+                 return mrcal_stereo_range_sparse(// output
+                   (double*)data_slice__output,
+
+                   // input
+                   (double*)data_slice__disparity,
+                   (const mrcal_point2_t*)data_slice__qrect0,
+                   N,
+
+                   *(double*)data_slice__disparity_min,
+                   *(double*)data_slice__disparity_max,
+
+                   // models_rectified
+                   cookie->type,
+                   (double*)data_slice__fxycxy_rectified,
+                   *(double*)data_slice__baseline);
+'''
+
+            },
+)
+
+m.function( "_stereo_range_dense",
+            """Internal wrapper of mrcal_stereo_range_dense()
+""",
+
+            args_input       = ('disparity_scaled',
+                                'disparity_scale',
+                                'disparity_scaled_min',
+                                'disparity_scaled_max',
+                                'fxycxy_rectified',
+                                'baseline'
+                                ),
+            prototype_input  = (('H','W'),
+                                (),
+                                (),
+                                (),
+                                (4,),
+                                ()),
+            prototype_output = ('H','W'),
+
+            extra_args = (("const char*", "rectification_model_type", "NULL", "s"),),
+
+            Ccode_cookie_struct = '''
+              mrcal_lensmodel_type_t type;
+            ''',
+
+            Ccode_validate = r'''
+
+              cookie->type = mrcal_lensmodel_type_from_name(rectification_model_type);
+              // the mrcal_stereo_range_...() functions will check to make sure
+              // the type is valid
+
+
+              if(strides_slice__disparity_scaled[1] != sizeof_element__disparity_scaled)
+              {
+                  PyErr_Format(PyExc_RuntimeError,
+                               "Array 'disparity_scaled' must be contiguous in the last dimension");
+                  return false;
+              }
+
+              if( strides_slice__output[1] != sizeof_element__output )
+              {
+                  PyErr_Format(PyExc_RuntimeError,
+                               "Array 'output' must be contiguous in the last dimension");
+                  return false;
+              }
+
+              return CHECK_CONTIGUOUS_AND_SETERROR_ALL();''',
+
+            Ccode_slice_eval = \
+            {
+                (np.uint16,np.uint16,np.uint16,np.uint16,float,float,
+                 float): '''
+
+                 const int H = dims_slice__disparity_scaled[0];
+                 const int W = dims_slice__disparity_scaled[1];
+
+                 return mrcal_stereo_range_dense(
+                   // output
+                   &(mrcal_image_double_t){.height = H,
+                                           .width  = W,
+                                           .stride = strides_slice__output[0],
+                                           .data   = data_slice__output},
+
+
+                   // input
+                   &(mrcal_image_uint16_t){.height = H,
+                                           .width  = W,
+                                           .stride = strides_slice__disparity_scaled[0],
+                                           .data   = data_slice__disparity_scaled},
+                   *(uint16_t*)data_slice__disparity_scale,
+                   *(uint16_t*)data_slice__disparity_scaled_min,
+                   *(uint16_t*)data_slice__disparity_scaled_max,
+
+                   // models_rectified
+                   cookie->type,
+                   (double*)data_slice__fxycxy_rectified,
+                   *(double*)data_slice__baseline);
+'''
+            },
+)
+
 m.write()

commit 5472c5fe0e4b1bdaed00cf14a1861262a6b03f23

mrcal.stereo_range() returns a scalar, if the input was a scalar
This fixes a regression introduced in 6f6212e740c61f9e16bcc69b1f38c6a7f5ffb6b0.
It also fixes a crash in mrcal-stereo: when interactively clicking of a point it
would say:

  TypeError: unsupported format string passed to numpy.ndarray.__format__

mrcal/stereo.py
@@ -1257,7 +1257,7 @@ RETURNED VALUES
         if np.any(disparity.shape - np.array((H,W),dtype=int)):
             raise Exception(f"qrect0 is None, so the disparity image must have the full dimensions of a rectified image")
 
-        return                                    \
+        r =                                    \
             mrcal._mrcal_npsp._stereo_range_dense \
                 ( disparity_scaled     = disparity.astype(np.uint16),
                   disparity_scale      = np.uint16(disparity_scale),
@@ -1269,7 +1269,7 @@ RETURNED VALUES
 
     else:
 
-        return                                     \
+        r =                                     \
             mrcal._mrcal_npsp._stereo_range_sparse \
                 ( disparity            = disparity.astype(float) / disparity_scale,
                   qrect0               = qrect0.astype(float),