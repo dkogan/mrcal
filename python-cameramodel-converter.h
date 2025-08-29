553f6b5f890ba04af069d60d48fd2d8ff4ce327a
Dima Kogan <dima@secretsauce.net>
2025-08-29 16:03:19 -0700

    #include in a published file uses "" instead of <>
    To pacify the compiler

--- python-cameramodel-converter.h
+++ python-cameramodel-converter.h
@@ -5,7 +5,7 @@
 // path strings or mrcal.cameramodel objects as mrcal_cameramodel_t structures
 
 #include <Python.h>
-#include <mrcal.h>
+#include "mrcal.h"
 
 int mrcal_cameramodel_converter(PyObject*             py_model,
                                 mrcal_cameramodel_t** model);
