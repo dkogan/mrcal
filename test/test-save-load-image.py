dd504f27a37e4998dfee0e0219f5d0188576deb0
Dima Kogan <dima@secretsauce.net>
2025-12-21 17:22:03 -0800

    test-save-load-image checks loading inconsistent types
    Like loading a 16-bit image as an 8-bit image

--- test/test-save-load-image.py
+++ test/test-save-load-image.py
@@ -46,24 +46,67 @@ images[24] *= images[24]
 filename = f"{workdir}/tst.png"
 
 
-def check_load(filename, image, what):
+def _check_load(filename, image_ref, what,
+                should_fail_load = False,
+                compare_value    = True,
+                **kwargs):
+
+    if should_fail_load:
+        testutils.confirm_raises(lambda: mrcal.load_image(filename, **kwargs),
+                                 msg = f"Loading {what} image should fail")
+        return
+
+
     try:
-        image_check = mrcal.load_image(filename)
-    except:
+        image_check = mrcal.load_image(filename, **kwargs)
+    except Exception as e:
         testutils.confirm(False,
-                          msg = f"Error loading {what} image")
+                          msg = f"Error loading {what} image: {e}")
+
         return
     testutils.confirm(True,
                       msg = f"Success loading {what} image")
 
-    # print(image.shape)
+    # print(image_ref.shape)
     # print(image_check.shape)
-    # print(image.dtype)
+    # print(image_ref.dtype)
     # print(image_check.dtype)
 
-    testutils.confirm_equal(image, image_check,
-                            worstcase=True,
-                            msg = f"load/save match for {what}")
+    bpp_load      = kwargs.get('bits_per_pixel')
+    channels_load = kwargs.get('channels')
+    if bpp_load is not None:
+        H,W = image_ref.shape[:2]
+        if bpp_load == 8:
+            testutils.confirm_equal(image_check.shape, (H,W),
+                                    msg = "Shapes match. Loading 8bpp-1channel image")
+            testutils.confirm_equal(image_check.ndim, 2,
+                                    msg = "channels match. Loading 8bpp-1channel image")
+            testutils.confirm_equal(image_check.dtype, np.uint8,
+                                    msg = "dtype match. Loading 8bpp-1channel image")
+        elif bpp_load == 16:
+            testutils.confirm_equal(image_check.shape, (H,W),
+                                    msg = "Shapes match. Loading 16bpp-1channel image")
+            testutils.confirm_equal(image_check.ndim, 2,
+                                    msg = "channels match. Loading 16bpp-1channel image")
+            testutils.confirm_equal(image_check.dtype, np.uint16,
+                                    msg = "dtype match. Loading 16bpp-1channel image")
+        elif bpp_load == 24:
+            testutils.confirm_equal(image_check.shape, (H,W,3),
+                                    msg = "Shapes match. Loading 24bpp-3channel image")
+            testutils.confirm_equal(image_check.ndim, 3,
+                                    msg = "channels match. Loading 24bpp-3channel image")
+            testutils.confirm_equal(image_check.dtype, np.uint8,
+                                    msg = "dtype match. Loading 24bpp-3channel image")
+
+    if compare_value:
+        if bpp_load == 24:
+            image_ref = nps.glue( nps.dummy(image_ref,-1),
+                                  nps.dummy(image_ref,-1),
+                                  nps.dummy(image_ref,-1),
+                                  axis = -1)
+        testutils.confirm_equal(image_ref, image_check,
+                                worstcase=True,
+                                msg = f"load/save match for {what}")
 
 
 
@@ -82,6 +125,41 @@ for bpp,what in ( (8,  "8bpp grayscale"),
     testutils.confirm(True,
                       msg = f"Success saving {what} image")
 
-    check_load(filename,image,what)
+    def check_load(**kwargs):
+        if 'bits_per_pixel' not in kwargs:
+            _check_load(filename, image,
+                        what,
+                        **kwargs)
+        else:
+            _check_load(filename, image,
+                        f"{what} (loading as {kwargs['bits_per_pixel']}bpp)",
+                        **kwargs)
+
+    check_load()
+
+    if bpp == 8:
+        check_load(bits_per_pixel = 16,
+                   channels       = 1,
+                   should_fail_load = True)
+        check_load(bits_per_pixel = 24,
+                   channels       = 3)
+
+    if bpp == 16:
+        check_load(bits_per_pixel = 8,
+                   channels       = 1,
+                   # values won't match: this path equalizes
+                   compare_value  = False)
+        check_load(bits_per_pixel = 24,
+                   channels       = 3,
+                   should_fail_load = True)
+
+    if bpp == 24:
+        check_load(bits_per_pixel = 8,
+                   channels       = 1,
+                   # values won't match: colors are collapsed
+                   compare_value  = False)
+        check_load(bits_per_pixel = 16,
+                   channels       = 1,
+                   should_fail_load = True)
 
 testutils.finish()
