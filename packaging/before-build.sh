35abc16e02d31aa8f51c1b00e5c36c49a558996c
Dima Kogan <dima@secretsauce.net>
2026-08-17 07:58:42 -0700

    pip: Claude is random-walking

--- packaging/before-build.sh
+++ packaging/before-build.sh
@@ -30,7 +30,6 @@ else
     export CPATH="${BUILD_DEPS}/include${CPATH:+:$CPATH}"
     export LIBRARY_PATH="${BUILD_DEPS}/lib${LIBRARY_PATH:+:$LIBRARY_PATH}"
     export LD_LIBRARY_PATH="${BUILD_DEPS}/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
-    export LDFLAGS="${LDFLAGS:+$LDFLAGS }-Wl,-rpath=${BUILD_DEPS}/lib"
     export PKG_CONFIG_PATH="${BUILD_DEPS}/lib/pkgconfig${PKG_CONFIG_PATH:+:$PKG_CONFIG_PATH}"
     export SWIG_FLAGS="-I${BUILD_DEPS}/include"
 fi
@@ -69,6 +68,6 @@ if [ "$(uname)" != "Darwin" ]; then ldconfig; fi
 # ---------------------------------------------------------------------------
 # mrgingham
 # ---------------------------------------------------------------------------
-make -C /tmp/mrgingham -j"${NCPUS}"
+LDFLAGS="-Wl,-rpath=${BUILD_DEPS}/lib" make -C /tmp/mrgingham -j"${NCPUS}"
 make -C /tmp/mrgingham install DESTDIR="${BUILD_DEPS}" ${INSTALL_ROOTS}
 if [ "$(uname)" != "Darwin" ]; then ldconfig; fi
